171e4fffd6f82bf061839990361565456b431e8f
Bill Boese <bill.boese@gmail.com>
2022-12-31 14:23:36 -0500

    new file for testing

---
+++ tester.py
@@ -0,0 +1,43 @@
+import pandas as pd
+
+gs = [5,2]
+gs = [[' ', ' '],[' ',' '],[' ',' '],[' ',' '],[' ',' ']]
+
+def ansFn(guess, answer, anslist):
+
+    # build the first column in the 2d array with just the letter
+    # do the color after
+
+    pl =[True, True, True, True, True]
+
+    for x in range(5):
+        anslist[x][0] = guess[x]
+
+    for x in range(5):  #green loop
+        if guess[x] == answer[x]:  #easy it is green
+            anslist[x][1] = 'green'
+            pl[x] = False
+
+    for x in range(5):  #gray loop
+        if pl[x]:   # this position is not green
+            m = False
+            for y in range(5):
+                if pl[y]:
+                    if guess[x] in answer[y]:
+                        m = True  # m starts false if it is never found it is gray
+            if not m:
+                anslist[x][1] = 'gray'
+            else:                         #does this work?
+                anslist[x][1] = 'yellow'
+
+    #check if all positions are green, ie its solved
+    print(pl)
+    if not any(pl):
+        return True
+    else:
+        return False
+
+if ansFn("mummm", "mummy", gs):
+    print("it returned true")
+
+print(gs)
